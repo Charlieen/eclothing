44c0920d2865e05ec08733ebdce40b1a4233d359
Charlieen <charliedding@gmail.com>
2020-04-27 10:38:16 -0400

    config

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,3 +1,9 @@
 export  const config= {
-
+    apiKey: "AIzaSyCB3fvh-a-He9slCNxrK4BxdCYxjqJu9yQ",
+    authDomain: "eclothing-9c86a.firebaseapp.com",
+    databaseURL: "https://eclothing-9c86a.firebaseio.com",
+    projectId: "eclothing-9c86a",
+    storageBucket: "eclothing-9c86a.appspot.com",
+    messagingSenderId: "427978181222",
+    appId: "1:427978181222:web:61b1b472d57638167d3ba7"
 }
